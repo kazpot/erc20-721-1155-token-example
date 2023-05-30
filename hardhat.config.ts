fbf6ebdca3b5820cc691aa7bec9474dc6074bc46
Kazuhiro Nakagawa <knman7@hotmail.com>
2023-05-30 14:25:17 +0900

    Fix typo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
-const { ETH_GOERLI_URL, POLYGON_MUMBAI_URL, OASYS_EMERALD_URL, PRIVATE_KEY } =
+const { ETH_GOERLI_URL, POLYGON_MUMBAI_URL, OASIS_EMERALD_URL, PRIVATE_KEY } =
   process.env;
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
@@ -18,7 +18,7 @@ const config: HardhatUserConfig = {
       accounts: [`0x${PRIVATE_KEY}`],
     },
     emerald: {
-      url: OASYS_EMERALD_URL,
+      url: OASIS_EMERALD_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
